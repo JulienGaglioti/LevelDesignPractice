f4b87d421ea24df816eebb57e04411a8438f2fbe
JulienGaglioti <39489340+JulienGaglioti@users.noreply.github.com>
2024-10-16 01:57:15 +0200

    CSV Utility

---
+++ Assets/CSVUtility/Example/ExampleCardSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Example Card SO")]
+public class ExampleCardSO : IdSO
+{
+    public string Name;
+    public int Cost;
+}
---
+++ Assets/CSVUtility/IdSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class IdSO : ScriptableObject
+{
+    // Scriptable Object con la sola proprietà di avere una stringa identificativa
+    public string Id;
+}
